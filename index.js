0d8ea38204e10a4ca6d380dc0f0b3b60074b33ad
Nehal Kadyan <nehalkadyan@Nehals-MacBook-Air.local>
2026-04-08 01:05:34 +0530

    test server

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use("/api", userRouter)
 app.use("/api/channel", channelRouter)
 
 app.get("/", (req, res) => {
-    return res.status(200).json({message : "Docker is working fine"})
+    return res.send("Welcome to YouTube Backend")
 })
 
 // video
